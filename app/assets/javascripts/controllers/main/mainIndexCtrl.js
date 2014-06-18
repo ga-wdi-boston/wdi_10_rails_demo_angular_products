35e5f091bd0f5c6b57f274b588f3b2b327026192
Tom Dyer <tdyer@ourstage.com>
2014-06-18 10:54:05 -0400

    Added an Angular controller for the Main page.

--- app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -1,4 +1,9 @@
 var IndexCtrl = function($scope){
-    $scope.title = "My Products";
-
-}
+    // Create a set of products
+    $scope.products = [
+        {name: 'Black Boot', description: 'We will crush you', price: 149.99},
+        {name: 'Television', description: '52 in', price: 599.00},
+        {name: 'Fishcake', description: 'Fish in a cake', price: 15.00},
+        {name: 'USB Plug', description: 'For your phone', price: 11.99}
+    ];
+};
