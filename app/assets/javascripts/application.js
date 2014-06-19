c2540ddb6c80d3c5a1a4b13a42f01467594192e5
Tom Dyer <tdyer@ourstage.com>
2014-06-19 14:34:15 -0400

    Cleanup sprockets directive to explicitly include js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,4 @@
 //= require jquery_ujs
 //= require angular
 //= require angular-route
-//= require_tree .
+
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+//= require_self
+//= require_tree ./controllers/main
+//= require_tree ./filters/main
+//= require_tree ./directives/main
+//= require_tree ./services/main
+
+alert('loading the main.js file');
