6523fcafbad58d2dd4127c87b20cca363416617a
Tom Dyer <tdyer@ourstage.com>
2014-06-19 12:03:23 -0400

    Created the main index angular controller

---
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -0,0 +1,13 @@
+//Create an index controller
+var IndexCtrl = function($scope){
+
+  // create a set of prouducts
+  $scope.products = [
+  {name: 'Black Boot', description: 'We will crush you', price: 149.99},
+  {name: 'Television', description: '52 in', price: 599.99},
+  {name: 'Fishcake', description: 'Fish in a cake', price: 15.00},
+  {name: 'USB Plug', description: 'For your phone', price: 11.99}
+  ];
+
+  
+}
