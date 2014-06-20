5c4ee64d92c2f4e65db203bf5e642a6584642534
Tom Dyer <tdyer@ourstage.com>
2014-06-20 07:59:29 -0400

    Added Ajax to retrieve the JSON for all products

--- app/assets/javascripts/controllers/main/mainIndexCtrl.js
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js
@@ -1,16 +1,32 @@
 //Create an index controller
-var IndexCtrl = function($scope){
+var IndexCtrl = function($scope, $http){
 
-  // create a set of prouducts
-  $scope.products = [
-  {name: 'Black Boot', description: 'We will crush you', price: 149.99},
-  {name: 'Television', description: '52 in', price: 599.99},
-  {name: 'Fishcake', description: 'Fish in a cake', price: 15.00},
-  {name: 'USB Plug', description: 'For your phone', price: 11.99}
-  ];
+    // create a set of prouducts
+    $scope.products = [
+        {name: 'Black Boot', description: 'We will crush you', price: 149.99},
+        {name: 'Television', description: '52 in', price: 599.99},
+        {name: 'Fishcake', description: 'Fish in a cake', price: 15.00},
+        {name: 'USB Plug', description: 'For your phone', price: 11.99}
+    ];
 
-  $scope.viewProduct = function(productId){
-    $location.url('/product/' + productId);
-  };
-  
+    $scope.viewProduct = function(productId){
+        $location.url('/product/' + productId);
+    };
+
+    productsHandler = function(data){
+        $scope.products = data
+        console.log('Successfully loaded products.')
+    };
+
+    errorHandler = function(){
+        console.error('Failed to load products.');
+    };
+
+    loadPosts = function(){
+        $http.get('./products.json')
+            .success(productsHandler)
+            .error(errorHandler)
+    };
+
+    loadPosts();  
 }
